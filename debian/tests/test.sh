9663bd6852757f3ca26f8de527424be12191651c
David Steele <dsteele@gmail.com>
2018-07-04 08:49:16 -0400

    Add more checking to autopkgtest

---
+++ debian/tests/test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+OUTDIR=${AUTOPKGTEST_ARTIFACTS:-"/tmp"}
+OUTFILE=${OUTDIR}/tstout
+
+./runtest.py 2>&1 >${OUTFILE}
+
+for result in 6 7 78 ; do
+  grep -q "result ${result}$" ${OUTFILE}
+  echo "Found result - ${result}"
+done
+
+if ! (wc -l <${OUTFILE} | grep -q ^3$ ) ; then
+  echo "Wrong number of lines in ${OUTFILE}"
+  exit 1
+fi
