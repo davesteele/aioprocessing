2635f9601c6a4c18f365d27edc20794f0d1dabf6
David Steele <steele@debian.org>
2018-10-23 21:12:47 -0400

    Release 1.0.1-3 - Fix autopkgtest

--- debian/tests/test.sh
+++ debian/tests/test.sh
@@ -5,7 +5,7 @@ set -e
 OUTDIR=${AUTOPKGTEST_ARTIFACTS:-"/tmp"}
 OUTFILE=${OUTDIR}/tstout
 
-./runtest.py 2>&1 >${OUTFILE}
+./debian/tests/runtest.py 2>&1 >${OUTFILE}
 
 for result in 6 7 78 ; do
   grep -q "result ${result}$" ${OUTFILE}
