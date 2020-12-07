76a79e4c9fd91edf136b150ac7cebaabd154fb06
David Steele <steele@debian.org>
2020-12-07 16:55:48 -0500

    Update autopkgtest to new demo test code

--- debian/tests/test.sh
+++ debian/tests/test.sh
@@ -7,12 +7,12 @@ OUTFILE=${OUTDIR}/tstout
 
 ./debian/tests/runtest.py 2>&1 >${OUTFILE}
 
-for result in 6 7 78 ; do
+for result in 6 7 8 9 10 78 ; do
   grep -q "result ${result}$" ${OUTFILE}
   echo "Found result - ${result}"
 done
 
-if ! (wc -l <${OUTFILE} | grep -q ^3$ ) ; then
+if ! (wc -l <${OUTFILE} | grep -q ^6$ ) ; then
   echo "Wrong number of lines in ${OUTFILE}"
   exit 1
 fi
